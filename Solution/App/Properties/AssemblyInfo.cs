4dfba210a2415e38ddb813d93a9e79c561f9d641
Burgy Benjamin <minidfx@gmail.com>
2016-02-10 21:38:01 +0100

    Update application metadatas.

--- Solution/App/Properties/AssemblyInfo.cs
+++ Solution/App/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("App2")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Bulk renaming")]
+[assembly: AssemblyDescription("Application for renaming many files using regex with a PREVIEW before doing the job.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("App")]
+[assembly: AssemblyProduct("Bulk renaming")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyFileVersion("0.1.0.*")]
 [assembly: ComVisible(false)]
