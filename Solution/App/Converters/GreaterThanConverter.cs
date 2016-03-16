f201a5ad72e1e087cc0b2bde21ae366200274df5
Burgy Benjamin <minidfx@gmail.com>
2016-03-16 21:02:04 +0100

    Add a new converter for determining when the application has to activated regex fields.

---
+++ Solution/App/Converters/GreaterThanConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Windows.UI.Xaml.Data;
+
+namespace BulkRenaming.Converters
+{
+    public sealed class GreaterThanConverter : IValueConverter
+    {
+        #region Interface Implementations
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var enumerable = value as IEnumerable<object>;
+            var greaterThan = ushort.Parse((string) parameter);
+
+            if (value != null)
+            {
+                return enumerable.Count() > greaterThan;
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
